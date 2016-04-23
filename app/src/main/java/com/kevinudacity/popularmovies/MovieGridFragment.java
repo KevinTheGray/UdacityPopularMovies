7e75e0dd6ae7d738258fd5bb48b089829f1f9e10
Kevin Gray <Kevin@GoPosse.com>
2016-04-23 18:59:47 -0400

    Committing before I start messing with real api key

--- app/src/main/java/com/kevinudacity/popularmovies/MovieGridFragment.java
+++ app/src/main/java/com/kevinudacity/popularmovies/MovieGridFragment.java
@@ -18,6 +18,8 @@ import android.widget.GridView;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -106,6 +108,36 @@ public class MovieGridFragment extends Fragment {
 
         URL url = new URL(builtUri.toString());
         Log.v(LOG_TAG, "" + url);
+
+        // Create the request to OpenWeatherMap, and open the connection
+        urlConnection = (HttpURLConnection) url.openConnection();
+        urlConnection.setRequestMethod("GET");
+        urlConnection.connect();
+
+        // Read the input stream into a String
+        InputStream inputStream = urlConnection.getInputStream();
+        StringBuffer buffer = new StringBuffer();
+        if (inputStream == null) {
+          // Nothing to do.
+          return null;
+        }
+        reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        String line;
+        while ((line = reader.readLine()) != null) {
+          // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+          // But it does make debugging a *lot* easier if you print out the completed
+          // buffer for debugging.
+          buffer.append(line + "\n");
+          Log.v(LOG_TAG, "" + moviesJsonStr);
+        }
+
+        if (buffer.length() == 0) {
+          // Stream was empty.  No point in parsing.
+          return null;
+        }
+        moviesJsonStr = buffer.toString();
+
       } catch (IOException e) {
 
       } finally {
