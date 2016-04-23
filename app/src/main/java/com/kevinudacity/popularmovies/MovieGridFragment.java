9495cc92993d95fb58d2148a54084b0912ae382d
Kevin Gray <Kevin@GoPosse.com>
2016-04-23 18:56:39 -0400

    Committing

--- app/src/main/java/com/kevinudacity/popularmovies/MovieGridFragment.java
+++ app/src/main/java/com/kevinudacity/popularmovies/MovieGridFragment.java
@@ -2,8 +2,11 @@ package com.kevinudacity.popularmovies;
 
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +15,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +37,12 @@ public class MovieGridFragment extends Fragment {
   }
 
   @Override
+  public void onStart() {
+    super.onStart();
+    fetchMovies("most_popular");
+  }
+
+  @Override
   public boolean onOptionsItemSelected(MenuItem item) {
     // Handle action bar item clicks here. The action bar will
     // automatically handle clicks on the Home/Up button, so long
@@ -38,6 +52,7 @@ public class MovieGridFragment extends Fragment {
       return true;
     }
     if (id == R.id.menu_sort_by_top_rated) {
+
       return true;
     }
     return super.onOptionsItemSelected(item);
@@ -63,4 +78,44 @@ public class MovieGridFragment extends Fragment {
     return rootView;
   }
 
+  // Helpers
+  private void fetchMovies(String sortValue) {
+    FetchMoviesTask fetchMoviesTask = new FetchMoviesTask();
+    fetchMoviesTask.execute(sortValue);
+  }
+
+  public class FetchMoviesTask extends AsyncTask<String, Void, String[]> {
+
+    private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+    @Override
+    protected String[] doInBackground(String... params) {
+      // These two need to be declared outside the try/catch
+      // so that they can be closed in the finally block.
+      HttpURLConnection urlConnection = null;
+      BufferedReader reader = null;
+
+      // Will contain the raw JSON response as a string.
+      String moviesJsonStr = null;
+
+      try {
+        Uri builtUri = Uri.parse(getString(R.string.moviedb_path_base)).buildUpon()
+          .appendPath(getString(R.string.moviedb_path_movie_most_popular))
+          .appendQueryParameter(getString(R.string.moviedb_query_param_api_key), "NOT REAL")
+          .build();
+
+        URL url = new URL(builtUri.toString());
+        Log.v(LOG_TAG, "" + url);
+      } catch (IOException e) {
+
+      } finally {
+
+      }
+      return null;
+    }
+
+    @Override
+    protected void onPostExecute(String[] result) {
+    }
+  }
 }
